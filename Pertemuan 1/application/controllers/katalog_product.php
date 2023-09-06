361a8ec5de341c3767bbc5b7387abcd370dec467
Askahhh <muhammadaskah61@gmail.com>
2023-09-06 16:31:43 +0700

    first commit

---
+++ Pertemuan 1/application/controllers/katalog_product.php
@@ -0,0 +1,16 @@
+<?php 
+class Katalog_product extends CI_Controller{
+//bagian yang ditambahkan
+function __construct() { 
+parent::__construct();
+$this->load->model('product_model');
+}
+public function index(){
+ $data['product']=$this->product_model->get_product();//bagian yang ditambahkan
+$this->load->view('header');
+$this->load->view('sidebar');
+$this->load->view('katalog_view',$data); //bagian yang ditambahkan
+$this->load->view('footer');
+ }
+}
+?>
